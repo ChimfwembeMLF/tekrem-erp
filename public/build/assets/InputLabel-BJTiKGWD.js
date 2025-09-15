41e565bccab74ef280bbc712cc565705f20a29f4
Chimfwembe Kangwa <chimfwembe.kangwa@mlfafrica.org>
2025-09-15 23:18:15 +0000

    patch

--- public/build/assets/InputLabel-BJTiKGWD.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./app-BMIA6nir.js";function m({value:t,htmlFor:e,children:r}){return s.jsx("label",{className:"block font-medium text-sm text-gray-700",htmlFor:e,children:t||r})}export{m as I};
--- public/build/assets/useTypedPage-hmuARwun.js
+++
@@ -1,1 +0,0 @@
-import{X as e}from"./app-BMIA6nir.js";function o(){return e()}export{o as u};
